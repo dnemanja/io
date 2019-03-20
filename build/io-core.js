adc7b0b95ab81c6c627879b37fb5dc7bf6742b71
AKI RODIC <aleksandar.xyz@gmail.com>
2019-03-20 14:49:59 +0100

    Build.

--- build/io-core.js
+++ build/io-core.js
@@ -855,6 +855,51 @@ function initStyle(prototypes) {
   }
 }
 
+if (!("serviceWorker" in navigator)) { console.error("No Service Worker support!"); }
+if (!("PushManager" in window)) { console.error("No Push API Support!"); }
+
+class IoServiceLoader extends IoNode {
+  static get properties() {
+    return {
+      path: 'service.js',
+      serviceWorker: undefined,
+      granted: window.Notification.permission === 'granted',
+      subscription: '',
+    };
+
+  }
+  constructor(props) {
+    super(props);
+    this.init();
+  }
+  async init() {
+    const serviceWorkerRegistration = await navigator.serviceWorker.register(this.path);
+    serviceWorkerRegistration.update();
+    navigator.serviceWorker.addEventListener('message', this.onServiceWorkerMessage);
+    if (serviceWorkerRegistration.active) {
+      this.serviceWorker = serviceWorkerRegistration;
+    } else {
+      serviceWorkerRegistration.addEventListener('activate', () => { this.serviceWorker = serviceWorkerRegistration; });
+    }
+  }
+  serviceWorkerChanged() {
+    if (this.granted) this.subscribe();
+  }
+  subscribe() {
+    if (navigator.serviceWorker.controller) {
+      navigator.serviceWorker.controller.postMessage({command: 'subscribe'});
+    }
+  }
+  async requestNotification() {
+    this.granted = await window.Notification.requestPermission() === 'granted';
+    if (this.granted) this.subscribe();
+  }
+  onServiceWorkerMessage(event) {
+    const data = JSON.parse(event.data);
+    if (data.subscription) this.subscription = JSON.stringify(data.subscription);
+  }
+}
+
 const nodes = {};
 let hashes = {};
 
@@ -946,50 +991,6 @@ function IoStorage(key, defValue, hash) {
     nodes[key].valueChanged();
   }
   return nodes[key].binding;
-}
-
-if (!("serviceWorker" in navigator)) { console.error("No Service Worker support!"); }
-if (!("PushManager" in window)) { console.error("No Push API Support!"); }
-
-const db = window.firebase !== undefined ? window.firebase.firestore() : null;
-
-class IoServiceLoader extends IoNode {
-  static get properties() {
-    return {
-      path: 'service.js',
-      serviceWorker: null,
-      granted: window.Notification.permission === 'granted',
-      subscription: IoStorage('io-notification-subscription'),
-    };
-  }
-  constructor(props) {
-    super(props);
-    this.requestService();
-  }
-  async requestService() {
-    if (this.granted) {
-      const serviceWorker = await navigator.serviceWorker.register(this.path);
-      serviceWorker.update();
-      if (!serviceWorker.active) {
-        serviceWorker.addEventListener('activate', () => { this.serviceWorker = serviceWorker; });
-      } else {
-        this.serviceWorker = serviceWorker;
-      }
-      navigator.serviceWorker.addEventListener('message', this.onServiceWorkerMessage);
-    }
-  }
-  async requestNotification() {
-    this.granted = await window.Notification.requestPermission() === 'granted';
-    this.requestService();
-  }
-  subscriptionChanged(event) {
-    if (db && this.subscription) db.collection("subscriptions").add({value: this.subscription});
-    if (db && event.detail.oldValue) db.collection("oldsubscriptions").add({value: event.detail.oldValue});
-  }
-  onServiceWorkerMessage(event) {
-    const data = JSON.parse(event.data);
-    if (data.subscription) this.subscription = JSON.stringify(data.subscription);
-  }
 }
 
 /**
