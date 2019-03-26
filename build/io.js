fd63999dd1cbd215f829470408c6e02c363b752e
Aki Rodic <aleksandar.xyz@gmail.com>
2019-03-26 01:43:22 +0100

    New Build.

--- build/io.js
+++ build/io.js
@@ -863,7 +863,7 @@ class IoServiceLoader extends IoNode {
     return {
       path: 'service.js',
       serviceWorker: undefined,
-      granted: window.Notification.permission === 'granted',
+      granted: window.Notification && window.Notification.permission === 'granted',
       subscription: '',
     };
 
